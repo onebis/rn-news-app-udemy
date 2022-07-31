11f76fba29090dc24c3b3ce851c59a49f1d2737b
@seebis <seebis78@gmail.com>
2022-07-31 13:50:44 +0900

    tmp commit

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
-    return <AppNavigator />;
+    return (
+        <Provider store={store}>
+            <AppNavigator />
+        </Provider>
+    );
 }
